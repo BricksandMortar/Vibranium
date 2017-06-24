fcf70af8cd53dc8eb853374f16b905b906d97e8c
Arran France <arranf@gmail.com>
2017-06-24 17:25:25 +0100

    No longer replace the Github pages URL

--- script/deploy.sh
+++ script/deploy.sh
@@ -6,14 +6,6 @@ rm -rf ./_site || exit 0;
 ./script/cibuild.sh
 ./script/citest.sh
 
-#Rebuild with fixed URls
-rm -rf ./_site || exit 0;
-#Fix Github's URLs
-# find tutorial https://www.digitalocean.com/community/tutorials/how-to-use-find-and-locate-to-search-for-files-on-a-linux-vps
-# Sed tutorial http://www.grymoire.com/Unix/Sed.html#uh-1
-find ./ -name "*.html" -exec sed -i -r 's|\{\{(\s*)site.github.url(\s*)\}\}|{{site.baseurl}}|g' {} \;
-./script/cibuild.sh
-
 #Deploy to Github gh-pages
 cd $TRAVIS_BUILD_DIR/_site
 git init
